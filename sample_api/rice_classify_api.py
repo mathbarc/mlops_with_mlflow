bb31860c053fcb7bbd0e66284015e0f9c986c017
Matheus Barcelos de Oliveira <matheus.oliveira@syngenta.com>
2025-04-18 15:30:00 -0300

    updates

--- sample_api/rice_classify_api.py
+++ sample_api/rice_classify_api.py
@@ -8,13 +8,14 @@ import os
 import base64
 import numpy
 from flask_cors import CORS
+import time
 
 app = Flask(__name__)
 CORS(app)
 
 
 def readb64(uri):
-    encoded_data = uri.split(',')[1]
+    encoded_data = uri.split(",")[1]
     nparr = numpy.frombuffer(base64.b64decode(encoded_data), numpy.uint8)
     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
     return img
@@ -22,11 +23,20 @@ def readb64(uri):
 
 @app.route("/classify", methods=["POST"])
 def classify():
-    model = mlflow.pytorch.load_model(os.environ["MODEL_URI"])
-
+    model_uri = os.environ["MODEL_URI"]
+    app.logger.info(f"Retrieving model {model_uri}")
+    model = mlflow.pytorch.load_model(model_uri)
+    app.logger.info("Decoding image")
     imageBase64 = request.form.get("image")
     img = readb64(imageBase64)
-    return jsonify(model.predict(img))
+
+    app.logger.info("Inferring image")
+    start = time.time()
+    result = model.predict(img)
+    end = time.time()
+
+    app.logger.info(f"DONE - {end-start} s - {result}")
+    return jsonify(result)
 
 
 if __name__ == "__main__":
